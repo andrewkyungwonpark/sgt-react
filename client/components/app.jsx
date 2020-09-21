82eacf0f7660790a74d47b4b01a606756c780193
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-09-21 16:20:33 -0700

    Allowed user to view all grades

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import Header from './header';
+import GradeTable from './gradetable';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+    this.viewAllGrades = this.viewAllGrades.bind(this);
+  }
+
+  componentDidMount() {
+    this.viewAllGrades();
+  }
+
+  viewAllGrades() {
+    fetch('/api/grades')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          grades: data
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
-    return null;
+    return (
+      <div className="container">
+        <div className="row">
+          <Header text="Student Grade Table" />
+        </div>
+        <div className="row">
+          <GradeTable grades={this.state.grades} />
+        </div>
+      </div>
+    );
   }
 }
-
 export default App;
